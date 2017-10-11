21079d28677b8f781ff129c7bd9341ef8c5459f3
arman213@gmail.com <88929501@Am>
2017-10-11 19:43:58 -0400

    background & title

--- bin/helloworld.cpp
+++ bin/helloworld.cpp
@@ -6,6 +6,9 @@ int main(int argc, char** argv)
   // create main window
   sf::RenderWindow App(sf::VideoMode(800,600,32), "Agent P: Infiltration");
 
+  int screen_width = 800;
+  int screen_height = 600;
+
   // start main loop
   while(App.isOpen())
   {
@@ -20,6 +23,49 @@ int main(int argc, char** argv)
 
     // clear screen and fill with blue
     App.clear(sf::Color::Blue);
+
+    // background
+    sf::Texture background;
+    if (!background.loadFromFile("/Users/arman/CLionProjects/Group4Game/resource/background/resized_bg.png"))
+    {
+      // error...
+    }
+
+    sf::Sprite bg(background);
+
+    App.draw(bg);
+
+
+    // logo
+    sf::Texture logo;
+
+    if (!background.loadFromFile("/Users/arman/CLionProjects/Group4Game/resource/API.png"))
+    {
+      // error...
+    }
+
+    sf::Sprite logo_sprite(logo);
+
+    App.draw(logo_sprite);
+
+
+    // Tagline
+    sf::Font titleFont;
+
+    if (!titleFont.loadFromFile("/Users/arman/CLionProjects/Group4Game/resource/fonts/Adventure.otf"))
+    {
+      // error...
+    }
+
+    sf::Text title;
+    title.setFont(titleFont);
+
+    title.setString("Agent P: Infiltration");
+    title.setColor(sf::Color::White);
+    title.setCharacterSize(48);
+    title.setPosition(sf::Vector2f(screen_width/2 - title.getLocalBounds().width/2, screen_height/2));
+
+    App.draw(title);
 
     // display
     App.display();
