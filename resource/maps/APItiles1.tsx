a22e8454e8b6ef0db6f9e27f3f17ea9e48dcb8e7
ruoyinghao <rhao@email.wm.edu>
2017-10-23 12:59:04 -0400

    updated map and csv

--- resource/maps/APItiles1.tsx
+++ resource/maps/APItiles1.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="tiles" tilewidth="32" tileheight="32" tilecount="805" columns="35">
- <image source="fall2017/Game Dev/tiles.png" width="1120" height="736"/>
+ <image source="tiles.png" width="1120" height="736"/>
  <terraintypes>
   <terrain name="ladder" tile="317"/>
   <terrain name="wall1" tile="317"/>
