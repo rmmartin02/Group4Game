72227b68ccc68b8cde350f2657cee54d1f1557bc
Russell Makoa Martin <rmmartin02@email.wm.edu>
2017-11-07 14:47:50 -0500

    Every direction text on control screen

--- src/Screens/ControlsScreen.cpp
+++ src/Screens/ControlsScreen.cpp
@@ -41,19 +41,31 @@ ControlsScreen::ControlsScreen(){
     back.setFillColor(sf::Color::White);
     back.setCharacterSize(30);
     back.setPosition(sf::Vector2f(SCREEN_WIDTH/2 - back.getLocalBounds().width/2, SCREEN_HEIGHT-back.getLocalBounds().height-30));
-    /*
-    back.setSize(sf::Vector2f(200, 50));
-    back.setOutlineColor(sf::Color::White);
-    back.setFillColor(sf::Color::Transparent);
-    back.setOutlineThickness(5);
-    back.setPosition(SCREEN_WIDTH/2 - back.getLocalBounds().width/2, SCREEN_HEIGHT/2);
-     */
-    upText.setFont(textFont);
 
+    //have some sort of "selected" variable, change color of that option and put arrow next to it
+    upText.setFont(textFont);
     upText.setString("Up:");
     upText.setFillColor(sf::Color::White);
     upText.setCharacterSize(30);
     upText.setPosition(sf::Vector2f(SCREEN_WIDTH/2 - upText.getLocalBounds().width/2, tagline.getPosition().y+tagline.getLocalBounds().height+30));
+
+    downText.setFont(textFont);
+    downText.setString("down:");
+    downText.setFillColor(sf::Color::White);
+    downText.setCharacterSize(30);
+    downText.setPosition(sf::Vector2f(SCREEN_WIDTH/2 - downText.getLocalBounds().width/2, upText.getPosition().y+upText.getLocalBounds().height+30));
+
+    leftText.setFont(textFont);
+    leftText.setString("left:");
+    leftText.setFillColor(sf::Color::White);
+    leftText.setCharacterSize(30);
+    leftText.setPosition(sf::Vector2f(SCREEN_WIDTH/2 - leftText.getLocalBounds().width/2, downText.getPosition().y+downText.getLocalBounds().height+30));
+
+    rightText.setFont(textFont);
+    rightText.setString("right:");
+    rightText.setFillColor(sf::Color::White);
+    rightText.setCharacterSize(30);
+    rightText.setPosition(sf::Vector2f(SCREEN_WIDTH/2 - rightText.getLocalBounds().width/2, leftText.getPosition().y+leftText.getLocalBounds().height+30));
 
 }
 
@@ -65,6 +77,9 @@ void ControlsScreen::render(sf::RenderWindow *window){
     window->draw(tagline);
     window->draw(back);
     window->draw(upText);
+    window->draw(downText);
+    window->draw(leftText);
+    window->draw(rightText);
     window->display();
 
 }
