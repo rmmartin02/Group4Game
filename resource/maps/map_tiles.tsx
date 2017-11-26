81dc944901d4803392d2227cf11f2d2b2c262cd0
Russell Makoa Martin <rmmartin02@email.wm.edu>
2017-11-25 21:17:42 -0500

    Added Tiled files

---
+++ resource/maps/map_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="map_tiles" tilewidth="32" tileheight="32" tilecount="16" columns="4">
+ <image source="../Desktop/Game/Game/resource/maps/map_tiles.png" width="128" height="128"/>
+</tileset>
